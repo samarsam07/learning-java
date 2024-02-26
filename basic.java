41dad6b7ae8e023c3e26dcc6c43eb3ca93007c71
samar <samarmohd251@gmail.com>
2024-02-27 00:07:22 +0530

    checking

--- basic.java
+++ basic.java
@@ -1,9 +1,14 @@
 import java.util.Scanner;
 public class basic {
     public static void main(String[] args){
-    //  Scanner scan=new Scanner(null);
-    //  int[] arr=new int[10]; 
-    
+     Scanner scan=new Scanner(System.in);
+     int[] arr=new int[10]; 
+    for(int i = 0 ; i<10 ; ++i){
+        arr[i] = scan.nextInt();
+    }
+    for(int i : arr){
+        System.out.println(i);
+    }
 
     }
     
